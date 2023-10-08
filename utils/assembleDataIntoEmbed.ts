4505850427ea160cacb15658cdd3bb7dd7d69717
anthony-urbina <aurbinava002@gmail.com>
2023-10-08 13:44:35 -0700

    added date to review card. added total reviews to title

--- utils/assembleDataIntoEmbed.ts
+++ utils/assembleDataIntoEmbed.ts
@@ -1,6 +1,7 @@
 import { Review } from "@prisma/client";
 import { idToUsername } from "./idToUsername";
 import { EmbedType } from "discord-api-types/v10";
+import formatDate from "./formatDate";
 
 interface AssembleDataIntoEmbedProps {
   last3Reviews: Review[];
@@ -16,23 +17,48 @@ export const assembleDataIntoEmbed = ({
   receivedByUsername,
 }: AssembleDataIntoEmbedProps) => {
   const reviewBodies = last3Reviews.map((review) => review.body);
+  const reviewDates = last3Reviews.map((review) => formatDate(review.createdAt));
   const authorIds = last3Reviews.map((review) => review.writtenById);
   const authorUsernames = authorIds.map((id) => idToUsername(id));
 
+  const maxReviewsToShow = 3;
+
+  // Combine review bodies, dates, and author usernames into a single array of strings
+  const combinedReviews = reviewBodies
+    .map((body, index) => {
+      if (body) {
+        return `${body} -${authorUsernames[index]} _${reviewDates[index]}_`;
+      }
+      return null;
+    })
+    .filter(Boolean); // Filter out any null or undefined values
+
+  // Handle case where there are no reviews
+  if (combinedReviews.length === 0) {
+    combinedReviews.push("No reviews yet");
+  }
+
+  // Format the reviews for the embed description
+  const formattedReviews = combinedReviews
+    .slice(0, maxReviewsToShow)
+    .map((review, index) => `**${index + 1}.** ${review}`)
+    .join("\n");
+
+  const totalCount = Number(positiveCount) + Number(negativeCount);
   const embeds = [
     {
-      title: `${receivedByUsername} 's Reviews`,
-      description: `**Latest Reviews:**\n**1.** ${reviewBodies[0]} -${authorUsernames[0]}\n**2.** ${reviewBodies[1]} -${authorUsernames[1]}\n**3.** ${reviewBodies[2]} -${authorUsernames[2]}`,
+      title: `${receivedByUsername}'s Reviews (${totalCount.toString()})`,
+      description: `**Latest:**\n${formattedReviews}`,
       color: 3980363,
       type: "rich",
       fields: [
         {
-          name: "Positive",
+          name: "Positive 👍",
           value: positiveCount,
           inline: true,
         },
         {
-          name: "Negative",
+          name: "Negative 👎",
           value: negativeCount,
           inline: true,
         },
